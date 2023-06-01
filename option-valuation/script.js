81469179f87a90fcb5c013c760ab263cf14f5dfb
skossovs <stan.kossovskiy@gmail.com>
2023-05-31 20:23:07 -0400

    code clean up

--- option-valuation/script.js
+++ option-valuation/script.js
@@ -19,7 +19,7 @@ const txtProjectedPrice = document.getElementById("projected-input");
 const txtProjectedPricePercenatage = document.getElementById("projected-percentage-input");
 const txtDelta = document.getElementById("option-delta-input");
 const txtTheta = document.getElementById("option-theta-input");
-
+// second leg fields
 const chkSecondLeg = document.getElementById("second-leg");
 const selCallPut2 = document.getElementById("call-put-select-2");
 const selBuySell2 = document.getElementById("buy-sell-select-2");
@@ -33,7 +33,6 @@ txtProjectedPrice.addEventListener("input", updateProjectedPricePercenatage);
 
 const colorsArray = ["rgb(241,42,7)", "rgb(241,42,7,0.8)", "rgb(241,42,7,0.6)", "rgb(241,42,7,0.4)", "rgb(241,42,7,0.2)", "rgb(11,7,243)", "rgb(21,151,4,0.2)", "rgb(21,151,4,0.4)", "rgb(21,151,4,0.6)", "rgb(21,151,4,0.8)", "rgb(21,151,4)" ];
 
-
 function updateProjectedPricePercenatage(){
   let calPutMult = selCallPut.value;
   txtProjectedPricePercenatage.value = (calPutMult * 100 * (txtProjectedPrice.value - txtSpot.value)/txtSpot.value).toFixed(2);
@@ -43,7 +42,6 @@ function updateExpirationDays(){
     let maturity = txtExpiration.value;
     var dmaturity = Date.parse(maturity);
     let dnow  = new Date(Date.now());
-
     let diffTime = Math.abs(dmaturity - dnow);
     let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
     txtDaysToExpiration.value = diffDays;
@@ -52,19 +50,16 @@ function updateExpirationDays(){
 function openSeries(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
-
   // Get all elements with class="tabcontent" and hide them
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
-
   // Get all elements with class="tablinks" and remove the class "active"
   tablinks = document.getElementsByClassName("tablinks");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
-
   // Show the current tab, and add an "active" class to the button that opened the tab
   document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
@@ -152,14 +147,10 @@ class OptionsViewModel {
 
     return { price: Price, delta: Delta, theta: Theta };
   }
-
-
 }
 
 // ------------- CALCULATIONS -----------------------------------------------------------------------------------------
 function render(){
-    
-  // CREATE CLASS
   const optionsViewModel = new OptionsViewModel();
 
   if(Number(txtHoldingPeriod.value) > Number(txtDaysToExpiration.value)) {
@@ -183,40 +174,39 @@ function render(){
     // Display graphs
   let LC = GenerateLabelsAndColors();
 
-    // Stock Series
-    let stockSeries = GenerateStockSeries();
-    let ctx = document.getElementById('StockSeriesChart');
-    const labels = stockSeries.X;
-    let cfg = CreateConfig();
-    AddLabels(cfg, labels);
-    for(i = 0; i<stockSeries.Y.length; i++){
-      AddData(cfg, stockSeries.Y[i], LC.Colors[i], LC.Labels[i]);
-    }
-    chartStocks = new Chart(ctx,cfg);
-    
-    // Option Series
-    let optionSeries = GenerateOptionSeries(optionsViewModel, stockSeries);
-    let ctxOption = document.getElementById('OptionSeriesChart');
-    let cfgOption = CreateConfig();
-    AddLabels(cfgOption, labels);
-    for(i = 0; i<optionSeries.Y.length; i++){
-      AddData(cfgOption, optionSeries.Y[i], LC.Colors[i], LC.Labels[i]);
-    }
-    chartOptions = new Chart(ctxOption,cfgOption);
+  // Stock Series
+  let stockSeries = GenerateStockSeries();
+  let ctx = document.getElementById('StockSeriesChart');
+  const labels = stockSeries.X;
+  let cfg = CreateConfig();
+  AddLabels(cfg, labels);
+  for(i = 0; i<stockSeries.Y.length; i++){
+    AddData(cfg, stockSeries.Y[i], LC.Colors[i], LC.Labels[i]);
+  }
+  chartStocks = new Chart(ctx,cfg);
+  
+  // Option Series
+  let optionSeries = GenerateOptionSeries(optionsViewModel, stockSeries);
+  let ctxOption = document.getElementById('OptionSeriesChart');
+  let cfgOption = CreateConfig();
+  AddLabels(cfgOption, labels);
+  for(i = 0; i<optionSeries.Y.length; i++){
+    AddData(cfgOption, optionSeries.Y[i], LC.Colors[i], LC.Labels[i]);
+  }
+  chartOptions = new Chart(ctxOption,cfgOption);
 
-    // Option PnLSeries
-    let optionPnLSeries = GenerateOptionPnLSeries(optionsViewModel, optionSeries);
-    let ctxOptionPnl = document.getElementById('OptionPnLSeriesChart');
-    let cfgOptionPnl = CreateConfig();
-    AddLabels(cfgOptionPnl, labels);
-    for(i = 0; i<optionPnLSeries.Y.length; i++){
-      AddData(cfgOptionPnl, optionPnLSeries.Y[i], LC.Colors[i], LC.Labels[i]);
-    }
-    chartOptionPnLs = new Chart(ctxOptionPnl,cfgOptionPnl);
+  // Option PnL Series
+  let optionPnLSeries = GenerateOptionPnLSeries(optionsViewModel, optionSeries);
+  let ctxOptionPnl = document.getElementById('OptionPnLSeriesChart');
+  let cfgOptionPnl = CreateConfig();
+  AddLabels(cfgOptionPnl, labels);
+  for(i = 0; i<optionPnLSeries.Y.length; i++){
+    AddData(cfgOptionPnl, optionPnLSeries.Y[i], LC.Colors[i], LC.Labels[i]);
+  }
+  chartOptionPnLs = new Chart(ctxOptionPnl,cfgOptionPnl);
 
-    // Cross Table
-    GenerateCrossTable(optionsViewModel);
-    
+  // Cross Table
+  GenerateCrossTable(optionsViewModel);
 }
 
 function GenerateLabelsAndColors(){
@@ -278,38 +268,11 @@ function calcOptionMetrics(S,K,sigma,T,r,q,bs){
   let sqrt2 = Math.sqrt(2);
   let d1 = (Math.log(K/S) + T * (r - q + (sigma*sigma)/2))/(sigma*Math.sqrt(T));
   let d2 = d1 - sigma * Math.sqrt(T);
-
-  // ************ THIS IF is FOR DEBUGGING PURPOSES ONLY *******************************
-  if(txtSpot.value == txtProjectedPrice.value){
-    console.info("d1,d2");
-    console.info(d1);console.info(d2);
-  }
-
   let Nd1_derivative = (1/Math.sqrt(2*Math.PI)) * Math.exp(-d1*d1/2);
-  let Nd1       = 0.5 + 0.5 * math.erf(d1/sqrt2);
-  let Nd2       = 0.5 + 0.5 * math.erf(d2/sqrt2);
-
-  // ************ THIS IF is FOR DEBUGGING PURPOSES ONLY *******************************
-  if(txtSpot.value == txtProjectedPrice.value){
-   console.info("Nd1,Nd2");
-   console.info(Nd1); console.info(Nd2);
-  }
-  let Nd1_minus = 0.5 + 0.5 * math.erf(-d1/sqrt2);
-  let Nd2_minus = 0.5 + 0.5 * math.erf(-d2/sqrt2);
-    
-  // ************ THIS IF is FOR DEBUGGING PURPOSES ONLY *******************************
-  if(txtSpot.value == txtProjectedPrice.value){
-
-    console.info("-Nd1,-Nd2");
-    console.info(Nd1_minus); console.info(Nd2_minus);
-    console.info("Exponents  e-rt, ke-rt, e-qt");
-    console.info(Math.exp(-r * T));
-    console.info(S * Math.exp(-r * T));
-    console.info(Math.exp(-q * T));
-    console.info("Exponents  Se-qt");
-    console.info(K*Math.exp(-q * T));
-  }
-
+  let Nd1            = 0.5 + 0.5 * math.erf(d1/sqrt2);
+  let Nd2            = 0.5 + 0.5 * math.erf(d2/sqrt2);
+  let Nd1_minus      = 0.5 + 0.5 * math.erf(-d1/sqrt2);
+  let Nd2_minus      = 0.5 + 0.5 * math.erf(-d2/sqrt2);
   let C  = K * Math.exp(-q * T) * Nd1       - S * Math.exp(-r * T) * Nd2;
   let P  = S * Math.exp(-r * T) * Nd2_minus - K * Math.exp(-q * T) * Nd1_minus;
   //https://www.macroption.com/black-scholes-formula/#delta
@@ -492,23 +455,12 @@ function GenerateStockSeries()
 function GenerateOptionSeries(optionsViewModel, stockSeries){
   let daysToExpiration = Number(txtDaysToExpiration.value);
   let holdingPeriod    = Number(txtHoldingPeriod.value);
-  let secondLeg        = chkSecondLeg.checked;
-  let S   = txtStrike.value;
-  let S2  = txtStrike2.value;
-  let bs  = selBuySell.value;
-  let bs2 = selBuySell2.value;
-  let r = txtInterest.value / 100;
-  let sigma = txtVolatility.value / 100;
-  let sigma2 = txtVolatility2.value / 100;
-  let calPutMult = selCallPut.value;
-  let calPutMult2 = selCallPut2.value;
 
   let Series = { X : [], Y : [null, null, null, null, null, null, null, null, null, null, null]};
   if (holdingPeriod == daysToExpiration) {
     for(x = 0; x <= daysToExpiration; x++) {
       Series.X.push(x);
     }
-
     for(let i = 0; i < nLines; i++){
       let littleSeries = [];
       for(x = 0; x <= daysToExpiration; x++) {
@@ -523,7 +475,6 @@ function GenerateOptionSeries(optionsViewModel, stockSeries){
     for(x = 0; x <= holdingPeriod; x++) {
       Series.X.push(x);
     }
-    // let expirationLeftOver = daysToExpiration - holdingPeriod;
     for(let i = 0; i < nLines; i++){
       let littleSeries = [];
       for(x = 0; x <= holdingPeriod; x++) {
@@ -537,38 +488,6 @@ function GenerateOptionSeries(optionsViewModel, stockSeries){
 
   return Series;
 }
-
-// function CalcInitialOptionPrice(){
-//   let S = txtStrike.value;
-//   let K = txtSpot.value;
-//   let bs = selBuySell.value;
-//   let r = txtInterest.value / 100;
-//   let sigma = txtVolatility.value / 100;
-//   let T = txtDaysToExpiration.value;
-//   let calPutMult = selCallPut.value;
-//   let secondLeg        = chkSecondLeg.checked;
-//   let S2  = txtStrike2.value;
-//   let bs2 = selBuySell2.value;
-//   let sigma2 = txtVolatility2.value / 100;
-//   let calPutMult2 = selCallPut2.value;
-
-//   let calcResultString = calcOptionMetrics(S,K,sigma,T/365,r,0,bs);
-//   let objResult        = JSON.parse(calcResultString);
-//   let Price = objResult.C;
-//   if(calPutMult == "-1")
-//     Price = objResult.P;
-
-//   if(secondLeg) {
-//     calcResultString = calcOptionMetrics(S2,K,sigma2,T/365,r,0,bs2);
-//     objResult        = JSON.parse(calcResultString);
-//     let P2 = objResult.C;
-//     if(calPutMult2 == "-1")
-//       P2 = objResult.P;
-//     Price = Price + P2;
-//   } 
-
-//   return Price;
-// }
 
 function GenerateOptionPnLSeries(optionsViewModel, optionSeries) {
   let daysToExpiration = Number(txtDaysToExpiration.value);
@@ -609,18 +528,6 @@ getMethods = (obj) => Object.getOwnPropertyNames(obj).filter(item => typeof obj[
 
 function GenerateCrossTable(optionsViewModel) {
   let Spot0 = txtSpot.value;
-  let S = txtStrike.value;
-  let bs = selBuySell.value;
-  let r = txtInterest.value / 100;
-  let sigma = txtVolatility.value / 100;
-  let calPutMult = selCallPut.value;
-
-  let daysToExpiration = Number(txtDaysToExpiration.value);
-  let holdingPeriod    = Number(txtHoldingPeriod.value);
-  let expirationLeftOver = 0;
-  if(holdingPeriod < daysToExpiration)
-    expirationLeftOver = daysToExpiration - holdingPeriod;
-
   const crossTable = document.getElementById("cross-table");
   // clear old data
   crossTable.innerHTML = "";
@@ -629,7 +536,6 @@ function GenerateCrossTable(optionsViewModel) {
   let InitialOptionPrice = optObject.price;  // initial option price
   let header = crossTable.createTHead();
   let row = header.insertRow(0);
-
   // Y - price, X - days
   // columns is being populated with stock prices
   axisSeries.Y.forEach( item => {
@@ -653,18 +559,6 @@ function GenerateCrossTable(optionsViewModel) {
         spot = 0.01;
 
       let objResult = optionsViewModel.CalcOptionMetrics(spot, days);
-
-      // let calcResultString = calcOptionMetrics(S, spot, sigma, (days + expirationLeftOver)/365, r, 0, bs);
-      // let objResult        = JSON.parse(calcResultString);
-      
-      // TODO: sometimes result is null. need to test
-      // let P = objResult.C ?? 0.00;
-      // if(calPutMult == "-1")
-      //   P = objResult.P ?? 0.00;  
-      // let Delta = objResult.Dc ?? 0.00;
-      // if(calPutMult == "-1")
-      //   Delta = objResult.Dp ?? 0.00;
-
       let P = objResult.price;
       let Delta = objResult.delta;
 
